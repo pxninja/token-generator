830fea9609f339d9c12f635415ccd637fbba8293
Samuel Davidson <samuel@pixelninja.com>
2024-07-05 18:08:08 -0500

    Context menu control
    Added ability to disable the context menu entry.

--- token-generator.py
+++ token-generator.py
@@ -16,16 +16,22 @@ import random
 
 class NewTokenCommand(sublime_plugin.TextCommand):
 
+  def __init__(self, *args, **kwargs):
+    super().__init__(*args, **kwargs)
+    self.settings = sublime.load_settings('token-generator.sublime-settings')
+
+  def is_visible(self):
+    return self.settings.get('show_in_context_menu', default = True)
+
   def run(self, edit): 
-    settings = sublime.load_settings('token-generator.sublime-settings')
-    string_length = settings.get('string_length', default=5)
-    character_set = settings.get('character_set', default='abcdef0123456789')
-    canBeAnything = not settings.get('force_alpha_numeric_inclusion', default=True)
+    string_length = self.settings.get('string_length', default = 5)
+    character_set = self.settings.get('character_set', default = 'abcdef0123456789')
+    homogeny_okay = not self.settings.get('force_alpha_numeric_inclusion', default = True)
     
     while True:
       token = ''.join(random.choice(character_set) for _ in range(string_length))
 
-      if canBeAnything or string_length <= 1:
+      if homogeny_okay or string_length <= 1:
         break
       elif any(c.isalpha() for c in token) and any(c.isdigit() for c in token):
         break
